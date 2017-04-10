01c97ec46b40ab5f2586e60239b83b8c749732b1
JonathanLR <jlr533@york.ac.uk>
2017-04-10 19:09:17 +0100

    test

--- app/src/main/java/com/example/work/dmm/DCvoltageActivity.java
+++ app/src/main/java/com/example/work/dmm/DCvoltageActivity.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +21,13 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -31,6 +40,8 @@ public class DCvoltageActivity extends AppCompatActivity {
     boolean isLogging = false;
     private int xoffset = 0;
     private float voltage=0;
+    private boolean currentVoltageLogged = false;
+
     private ArrayList<Entry> entry_list = new ArrayList<Entry>();
 
     private String[] unitsForRanges = {"V","mV","mV","mV"};
@@ -53,6 +64,8 @@ public class DCvoltageActivity extends AppCompatActivity {
                         genChart();
                     }
                     xoffset++;
+                }else{
+                    currentVoltageLogged = false;
                 }
             }else{//inside voltage activity but received wrong packet. send change mode packet
                 Intent change_mode = new Intent(MessageCode.DMM_CHANGE_MODE_REQUEST);
@@ -89,13 +102,17 @@ public class DCvoltageActivity extends AppCompatActivity {
         }
         return 0;
     }
+    /*DEBUG*/
+    final Handler h = new Handler();
+    final int delay = 100; //milliseconds
+/*DEBUG END*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dcvoltage);
         base = (BaseApplication)getApplicationContext();
-        logVoltageButton = (Button) findViewById(R.id.logVoltageButton);
+        logVoltageButton = (Button) findViewById(R.id.btn_ClearLog);
         gauge = (Speedometer) findViewById(R.id.gauge);
         gauge.setMax(10);
         gauge.setMin(-10);
@@ -107,57 +124,108 @@ public class DCvoltageActivity extends AppCompatActivity {
 
         //Line Chart
         loggingLineChart = (LineChart) findViewById(R.id.loggingLineChart);
-        loggingLineChart.setVisibility(View.INVISIBLE);
+        entry_list = new ArrayList<Entry>();
 
         //Data Logging Toggle
         Switch dataLogTog = (Switch) findViewById(R.id.loggingToggle);
         dataLogTog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggleLogging();
+                toggleAutoLogging();
             }
         });
 
+
+/*DEBUG*/
+        h.postDelayed(new Runnable(){
+            public void run(){
+                int range = r.nextInt(4);
+                float voltage = 0;
+                //values so that they are sometimes out of range
+                switch (range){
+                    case 0:
+                        voltage = (r.nextFloat()*15+r.nextFloat()*-15);
+                        break;
+                    case 1:
+                        voltage = (r.nextFloat()*1.5f+r.nextFloat()*-1.5f);
+                        break;
+                    case 2:
+                        voltage = (r.nextFloat()*0.15f+r.nextFloat()*-0.15f);
+                        break;
+                    case 3:
+                        voltage = (r.nextFloat()*0.015f+r.nextFloat()*-0.015f);
+                        break;
+                }
+                Log.e("blah","voltage="+voltage+" Range="+range);
+                Intent I = new Intent(MessageCode.PARSED_DATA_DC_VOLTAGE);
+                I.putExtra(MessageCode.VALUE,voltage);
+                I.putExtra(MessageCode.RANGE,range);
+                sendBroadcast(I);
+                h.postDelayed(this, delay);
+            }
+        }, delay);
+/*DEBUG END*/
+
     }
 
-    private void toggleLogging(){
-        //Hide appropriate UI elements within this method when not logging
-        if(!isLogging) {
-            entry_list = new ArrayList<Entry>();
-            loggingLineChart.setVisibility(View.VISIBLE);
-        } else{
-            loggingLineChart.setVisibility(View.INVISIBLE);
-        }
+    private void toggleAutoLogging(){
         isLogging = !isLogging;
     }
 
-    /*DEBUG CODE !
-    * EMULATES RECEIVING A PACKET CONTAINING VOLTAGE AND RANGE.*/
     public void logVoltage(View view){
-        int range = r.nextInt(4);
-        float voltage = 0;
-        //values so that they are sometimes out of range
-        switch (range){
-            case 0:
-                voltage = (r.nextFloat()*15+r.nextFloat()*-15);
-                break;
-            case 1:
-                voltage = (r.nextFloat()*1.5f+r.nextFloat()*-1.5f);
-                break;
-            case 2:
-                voltage = (r.nextFloat()*0.15f+r.nextFloat()*-0.15f);
-                break;
-            case 3:
-                voltage = (r.nextFloat()*0.015f+r.nextFloat()*-0.015f);
-                break;
+        if (!currentVoltageLogged) {
+            Entry e =new Entry((float)xoffset,voltage);
+            entry_list.add(e);
+            if(entry_list.size()>0){
+                genChart();
+            }
+            xoffset++;
+            currentVoltageLogged = true;
         }
-        Log.e("blah","voltage="+voltage+" Range="+range);
-        Intent I = new Intent(MessageCode.PARSED_DATA_DC_VOLTAGE);
-        I.putExtra(MessageCode.VALUE,voltage);
-        I.putExtra(MessageCode.RANGE,range);
-        sendBroadcast(I);
     }
-    /*/DEBUG CODE*/
+
+    public void onClickOscilloscopeMode(View view){
+
+    }
+
+    public void onClickExportData(View view){
+        File root   = Environment.getExternalStorageDirectory();
+        File dir    =   new File (root.getAbsolutePath() + "/DMM_Saved_Logs");
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        File file   =   new File(dir, "VoltageLog_"+currentDateTimeString+".txt");
+        StringBuilder dataString = new StringBuilder("");
+        ArrayList<Entry> copyList = (ArrayList<Entry>) entry_list.clone();
+        for(int i=0;i<copyList.size();i++){
+            String dataPoint = "x:"+copyList.get(i).getX()+" y:"+copyList.get(i).getY()+"\n";
+            dataString.append(dataPoint);
+        }
+        String data = dataString.toString();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            out.write(data.getBytes());
+            out.close();
+        } catch (Exception e) {
+            base.ts("ERROR encountered when trying to export data.");
+            e.printStackTrace();
+        }
+        // try to send file via email
+        Uri fileLocation  =   Uri.fromFile(file);
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "DMM Voltage data");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, fileLocation);
+        sendIntent.setType("text/html");
+        startActivity(sendIntent);
+    }
+
+    public void onClickClearLog(View view){
+        entry_list = new ArrayList<>();
+        loggingLineChart.clear();
+        loggingLineChart.invalidate();
+    }
+
 
     private void genChart() {
         //Generate new dataset
@@ -166,7 +234,7 @@ public class DCvoltageActivity extends AppCompatActivity {
         if (entries.size() > base.getMaxDataPointsToKeep()){
             entries.remove(0);
         }
-        LineDataSet dataSet = new LineDataSet(entries, "Shitty Point Data"); // add entries to dataset
+        LineDataSet dataSet = new LineDataSet(entries, "Voltage"); // add entries to dataset
         dataSet.setColor(ColorTemplate.COLORFUL_COLORS[1]);
         dataSet.setValueTextColor(Color.BLACK);
         LineData lineData = new LineData(dataSet);
