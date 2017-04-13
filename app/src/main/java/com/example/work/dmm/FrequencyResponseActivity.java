974d50ffbb4d1eaf35e117eb5f1f2268198c5385
JonathanLR <jlr533@york.ac.uk>
2017-04-13 11:20:47 +0100

    -> scratch the webview approach, found out that MPAndroidChart can do logplots after all via axisFormatter. Implemented the loglinear graph for the freqResp activity

--- app/src/main/java/com/example/work/dmm/FrequencyResponseActivity.java
+++ app/src/main/java/com/example/work/dmm/FrequencyResponseActivity.java
@@ -1,18 +1,47 @@
 package com.example.work.dmm;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import com.numetriclabz.numandroidcharts.LogarithmicLineChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class FrequencyResponseActivity extends AppCompatActivity {
-
+    private LineChart logchart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frequency_response);
-        LogarithmicLineChart logchart = (LogarithmicLineChart) findViewById(R.id.logLineChart);
-        logchart.setBase(10);
-
+        logchart = (LineChart) findViewById(R.id.freqRespGraph);
+        logchart.getXAxis().setValueFormatter(new Log10AxisValueFormatter());
     }
+
+    public void onClickBeginDataAquisition(View view){
+        List<Entry> entry_list = new ArrayList<>();
+        for(int i = 0; i<10;i++){
+            float yval = -i;
+            double xval = Math.pow(10,i);
+            Entry e =new Entry((float)Math.log10(xval),yval);
+            entry_list.add(e);
+        }
+        LineDataSet dataSet = new LineDataSet(entry_list, "Voltage"); // add entries to dataset
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+        LineData lineData = new LineData(dataSet);
+        logchart.setData(lineData);
+        logchart.invalidate(); // refresh
+        logchart.notifyDataSetChanged();//Causes redraw when we add data. I imagine we'll initiate
+    }
+
 }
